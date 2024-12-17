bc76a492fd3d99607faa250a2ff5e3c6f382bdba
Julijan Šribar <julijan.sribar@gmail.com>
2024-12-17 23:42:46 +0100

    Add project files.

---
+++ Storyteller/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Storyteller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Story story = new Story();
+            story.WriteHeading("Šuma Striborova");
+            story.WriteParagraph("Zašao neki momak u šumu Striborovu...");
+        }
+    }
+}
---
+++ Storyteller/Story.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Storyteller
+{
+    class Story
+    {
+        public void WriteHeading(string title)
+        {
+            ++section_number;
+
+            title = $"{section_number} {title}";
+            Console.WriteLine(title);
+            Console.WriteLine(new string('=', title.Length));
+        }
+
+        public void WriteParagraph(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        private int section_number = 0;
+    }
+}
